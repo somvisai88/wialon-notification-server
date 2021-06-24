9d3b8f26433046099ef9394ac12000c5bebce3cc
SOM Visai <visai.s@mangotracking.com>
2021-06-24 17:25:43 +0700

    5rd Commit

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-var request = require("request");
+const request = require("request");
 request.get("http://codeforgeek.com",function(error,response,body){
     if(error){
         console.log(error);
     }else{
         console.log(response);
     }
+});
+
+const express = require('express');
+const app = express();
+
+app.post('/callMe', function(req, res){
+    console.log("=======TEST=======");
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, ()=> {
+    console.log('Our app is running on port ${ PORT }');
 });
 
