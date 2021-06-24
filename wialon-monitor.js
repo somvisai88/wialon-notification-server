f7936425c4a923682aee970de2e200f527888c69
SOM Visai <visai.s@mangotracking.com>
2021-06-24 16:50:26 +0700

    2nd commit

---
+++ wialon-monitor.js
@@ -0,0 +1,8 @@
+var request = require("request");
+request.get("http://codeforgeek.com",function(error,response,body){
+    if(error){
+        console.log(error);
+    }else{
+        console.log(response);
+    }
+});
